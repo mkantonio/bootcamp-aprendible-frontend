015c6c8354c544eb26fe01711c228f8e03118f51
Marco Antonio <mkchoque@gmail.com>
2022-11-02 20:34:37 -0400

    creación de componentes dinámicos

---
+++ pages/libros/[bid]/[bid].js
@@ -0,0 +1,9 @@
+const BookDetail = () => {
+    return (
+        <h1>
+            Book 1
+        </h1>
+    )
+}
+
+export default BookDetail;
---
+++ pages/libros/[bid]/editar.js
@@ -0,0 +1,11 @@
+const BookEdit = () => {
+    return (
+        <h1>
+
+            BookEdit
+
+        </h1>
+    )
+}
+
+export default BookEdit;

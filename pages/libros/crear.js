315fb91aeae1e9dc6c10027d49c23538c771328a
Marco Antonio <mkchoque@gmail.com>
2022-11-03 14:00:35 -0400

    two way data binding

--- pages/libros/crear.js
+++ pages/libros/crear.js
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import {useState} from "react";
 
 const BookCreate = () => {
+
+    const [bookName, setBookName] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        console.log(bookName)
+    }
+
     return (
         <>
             <h1>BookCreate</h1>
-
+            <p>{bookName}</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e) => setBookName(e.target.value)}
+                    value={bookName}
+                    type="text"/>
+                <button>Enviar</button>
+            </form>
+            <br/>
             <Link href="/libros">Book List</Link>
         </>
 

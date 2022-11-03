f1074b8e48b55384dc509daf2f836907bc3de15a
Marco Antonio <mkchoque@gmail.com>
2022-11-03 19:33:42 -0400

    método editar funcionando ok

--- pages/libros/[bid]/editar.js
+++ pages/libros/[bid]/editar.js
@@ -1,11 +1,77 @@
-const BookEdit = () => {
-    return (
-        <h1>
+import Link from "next/link";
+import {useState} from "react";
+import {useRouter} from "next/router";
 
-            BookEdit
+export async function getServerSideProps({ params }){
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${params.bid}`);
+    const data = await res.json();
 
-        </h1>
-    )
+    return {
+        props: {
+            book: data,
+        },
+    };
 }
+
+const BookEdit = ({ book }) => {
+    const router = useRouter();
+    const [bookTitle, setBookTitle] = useState(book.title);
+    const [errors, setErrors] = useState([]);
+    const [submitting, setSubmitting] = useState(false);
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setSubmitting(true);
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${book.id}`,
+            {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: bookTitle,
+                _method: 'PATCH',
+            }),
+        });
+        if (res.ok){
+            setErrors([]);
+            setBookTitle('');
+            return router.push('/libros');
+
+        }
+        const data = await res.json();
+        setErrors(data.errors);
+        setSubmitting(false);
+    }
+
+    return (
+        <>
+            <h1>BookEdit</h1>
+            <p>{bookTitle}</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e) => setBookTitle(e.target.value)}
+                    value={String(bookTitle)}
+                    disabled={submitting}
+                    type="text"/>
+                <button disabled={submitting}>
+                    {submitting ? 'Enviando...' : 'Enviar'}
+                </button>
+                {errors.title && (
+                    <span style={{
+                        color: 'red', display: 'block'
+                    }}>{errors.title}</span>
+                )}
+
+            </form>
+            <br />
+            <Link href="/libros">Book List</Link>
+        </>
+
+    );
+};
 
 export default BookEdit;
